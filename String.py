5aa3a0c1fd0cfca7fc2e0dacaab179dd52c2b909
jsbaidwan <cgc.jsbaidwan@gmail.com>
2019-01-23 16:20:21 +0530

    String and its properties

---
+++ String.py
@@ -0,0 +1,44 @@
+# single quotes
+print('This is Python course')
+# double quotes
+print("This is Python course")
+
+# Escape the single quotes
+print("Doesn't know")
+print('Doesn\'t know')
+
+# New line
+print("First line \n Second line")
+
+# Escape the double quotes
+print('"Yes", he said')
+print("\"Yes\", he said")
+
+print('Py' 'charm')
+
+# String concatenate and repeated
+print(2 * "ass" + "ination")
+
+course_name = "Python Tutorial"
+print(course_name)
+
+# length of the string
+print(len(course_name))
+
+# Character at first index
+print(course_name[0])
+
+# Character at second last index
+print(course_name[-1])
+
+# First three character
+print(course_name[0:3])
+print(course_name[:3])
+
+# First to last character
+print(course_name[0:])
+print(course_name[:])
+
+# Immutable properties of String
+print(id(course_name))
+print(id(course_name[0]))
