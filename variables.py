55ba6a869c5f385da396807437d6401fc7f585b3
jsbaidwan <cgc.jsbaidwan@gmail.com>
2019-01-23 06:33:58 +0530

    variables in python

---
+++ variables.py
@@ -0,0 +1,22 @@
+# String
+class_name = "python"
+
+# multiple line String
+course_name = """This 
+course for
+developer"""
+
+# Integer
+count = 3
+
+# Boolean
+status = True
+
+x = 1
+y = 2
+
+# Initialize the above in one line
+x1, y1 = 1, 2
+
+x2 = y2 = 1
+
