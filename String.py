6ecc7c6aa853a5eec3de8c21e331a49f6f872e26
jsbaidwan <cgc.jsbaidwan@gmail.com>
2019-01-25 05:13:52 +0530

    String Foramtting

--- String.py
+++ String.py
@@ -3,6 +3,11 @@ print('This is Python course')
 # double quotes
 print("This is Python course")
 
+# Triple quotes for multiple lines
+print("""This
+is the 
+Python Course""")
+
 # Escape the single quotes
 print("Doesn't know")
 print('Doesn\'t know')
@@ -42,3 +47,32 @@ print(course_name[:])
 # Immutable properties of String
 print(id(course_name))
 print(id(course_name[0]))
+
+# Formatted String
+formatting = f"{len(course_name)} {2+1}"
+print(formatting)
+
+# String methods
+# Character Capitalize
+print(course_name.capitalize())
+
+# Character to lowercase
+print(course_name.lower())
+
+# First character of every word be capital
+print(course_name.title())
+
+# Remove the white space
+print(course_name.strip())
+
+# Find character of word
+print(course_name.find("thon"))
+
+# Replace character
+print(course_name.replace("P", "-"))
+
+# Check existence nonexistence
+print("Tutorial" in course_name)
+print("Tutorial" not in course_name)
+
+
