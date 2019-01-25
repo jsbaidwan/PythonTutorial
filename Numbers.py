dc6b3992f0012a4b52f1ad7c6fcae8364878d3ee
jsbaidwan <cgc.jsbaidwan@gmail.com>
2019-01-25 06:06:15 +0530

    Operators in numbers

---
+++ Numbers.py
@@ -0,0 +1,46 @@
+# Numbers
+
+# Decimal Number to Binary
+x = 5
+print(bin(x))
+
+# Binary to Decimal
+x = 0b111
+print(x)
+
+# Hexadecimal number
+x = 0x4f
+print(x)
+
+# Complex number
+x = 1 + 2j
+print(x)
+
+# Operators
+x = 1 + 2   # addition
+print(x)
+
+x = 3-2  # Subtraction
+print(x)
+
+x = 4*5  # Multiplication
+print(x)
+
+x = 10 / 3  # Division always return floating point number
+print(x)
+
+x = 10 // 3  # Division return as Integer
+print(x)
+
+x = 10 % 3  # Modulus
+print(x)
+
+# Exponent
+x = 7 ** 2  # left to the power of right
+print(x)
+
+# Assignment operators
+x += 3
+print(x)
+
+# No increment and decrement operators such as x-- or x++
