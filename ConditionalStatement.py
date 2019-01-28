0b1c3141cd2faebb84526b4ae476707c7508860d
jsbaidwan <cgc.jsbaidwan@gmail.com>
2019-01-28 06:10:00 +0530

    Operators

--- ConditionalStatement.py
+++ ConditionalStatement.py
@@ -1,8 +1,6 @@
 
-
-
-
 # If condition
+
 age = 22
 if age >= 18:
     print("Adult")
@@ -12,3 +10,35 @@ else:
     print("Child")
 
 print("done")
+
+# Statement block
+if age > 1:
+    pass   # for empty block use pass
+else:
+    pass
+
+# Not Operators
+name = "baidwan"
+if not name:
+    print("Not a name")
+
+# And Operators
+age = 22
+if age >= 18 and age < 60:
+    print("Eligible")
+# Chain comparison
+if 18 <= age < 60:
+    print("eligible")
+
+
+if age >= 18:
+    message = "Eligible"
+else:
+    message = "Not Eligible"
+
+# Ternary Operators for above Statement
+
+message = "Eligible " if age >= 18 else "Not Eligible"
+
+print(message)
+
