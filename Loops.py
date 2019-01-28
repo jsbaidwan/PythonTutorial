9f6f088c51cdf349e3718a3a101a1b398b13f3b5
jsbaidwan <cgc.jsbaidwan@gmail.com>
2019-01-28 07:04:40 +0530

    For Loop

---
+++ Loops.py
@@ -0,0 +1,28 @@
+# For loops
+
+# Iterate over String
+for x in ("Pyth"):
+    print(x)
+
+# Iterate over List
+for x in ('o', 'n'):
+    print(x)
+
+# Range function
+# Range function does not return list, its an object
+
+# class range(stop)
+for x in range(5):
+    print(x)
+
+# class range(start, stop)
+for x in range(2, 5):
+    print(x)
+
+# class range(start, stop[, step])
+# step as increment in loop
+for x in range(0, 10, 2):
+    print(x)
+
+
+
