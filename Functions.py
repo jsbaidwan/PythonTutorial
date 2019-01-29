7a3524796d09022da1fa975d3b0afadadd20b4f7
jsbaidwan <cgc.jsbaidwan@gmail.com>
2019-01-29 12:29:08 +0530

    functions

---
+++ Functions.py
@@ -0,0 +1,14 @@
+# functions to add integer number
+def addition(x, y):
+    return x + y
+
+
+print(addition(2, 3))
+
+
+# functions to add list
+def increment(number, lit):
+    return lit + number
+
+
+print(increment([4], [1, 2]))
