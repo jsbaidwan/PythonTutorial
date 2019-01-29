824dcf8a52649c83aacd9f10c623554c5076bf93
jsbaidwan <cgc.jsbaidwan@gmail.com>
2019-01-29 05:47:07 +0530

    for else loop

--- Loops.py
+++ Loops.py
@@ -24,5 +24,11 @@ for x in range(2, 5):
 for x in range(0, 10, 2):
     print(x)
 
-
-
+# For else
+name = ["john", "Mike"]
+for names in name:
+    if names.startswith("j"):
+        print("Found")
+        break   # to break the loop
+else:
+    print("Not found")
