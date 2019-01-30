fd594849645e3217366f16999cfd9fb03840447f
jsbaidwan <cgc.jsbaidwan@gmail.com>
2019-01-30 13:27:24 +0530

    passing tuple / list as argument

--- Functions.py
+++ Functions.py
@@ -12,3 +12,28 @@ def increment(number, lit):
 
 
 print(increment([4], [1, 2]))
+
+
+# Passing arguments
+
+# passing list as argument
+def add(lit):
+    for ad in lit:
+        return ad
+
+
+print(add([3, 1, 8]))
+
+# multiply the tuple
+
+
+def multiply(*num):   # * is used to define it as tuple
+    total = 1
+    for x in num:
+        total *= x
+    return total
+
+
+print(multiply(9, 2, 3))
+
+
