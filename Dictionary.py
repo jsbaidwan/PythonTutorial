2b8df2f2f69562579e5044ce846cdf6bb9935bf5
jsbaidwan <cgc.jsbaidwan@gmail.com>
2019-02-07 14:58:51 +0530

    check key value in dictionary

--- Dictionary.py
+++ Dictionary.py
@@ -9,8 +9,11 @@ print(fruits["Apple"])
 
 
 fruits["kiwi"] = "Brown"  # update dictionary
-print("Updated", fruits.items())
+print('Updated', fruits.items())
 
 del fruits["Banana"]
-print("Deleted", fruits.items())
+print('Deleted', fruits.items())
+
+print("Check for kiwi key value: ", 'kiwi' in fruits)
+
 
