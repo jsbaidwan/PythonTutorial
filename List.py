b59f9794368523acd8dcb40db418ddb287f39292
jsbaidwan <cgc.jsbaidwan@gmail.com>
2019-03-17 22:27:54 +0530

    list

---
+++ List.py
@@ -0,0 +1,14 @@
+# Data structures are containers that organize and group data types together in different ways.
+# A list is one of the most common and basic data structures in Python.
+
+list_of_random_things = [1, 3.4, 'a string', True]
+print(list_of_random_things[0])
+
+print(list_of_random_things[-1])
+
+print(list_of_random_things[-2])
+
+
+
+
+
